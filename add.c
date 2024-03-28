50f43ce3f4bf17a4b63fd5376969f6b44c31d47c
harper71 <princeazuka99@gmail.com>
2024-03-28 01:48:51 +0100

    for add in task(4) commit(2)

--- add.c
+++ add.c
@@ -9,15 +9,19 @@ void add(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (!stack || !*stack || !(*stack)->next)
+	if (stack == NULL || (*stack) == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	temp = *stack;
+
 	(*stack)->next->n += (*stack)->n;
+
 	*stack = (*stack)->next;
+
 	(*stack)->prev = NULL;
+
 	free(temp);
 }
